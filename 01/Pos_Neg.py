36a6dd84f5f0afc625ebc66da3193c8494c3dc8b
Jide Adebiyi <jeedeyy@gmail.com>
2020-07-13 19:05:47 +0100

    Create Pos_Neg.py
    This python code detects positive and negative numbers

---
+++ 01/Pos_Neg.py
@@ -0,0 +1,13 @@
+print('Hi there')
+print('insert number:')
+
+num = float(input())
+
+if num > 0:
+    print('the number is a POSITIVE number')
+elif num < 0:
+    print('the number is a NEGATIVE number')
+else:
+    print('number is equal to ZERO!')
+
+print('Thank you!')
