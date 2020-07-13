8cbce607a9d4bb11aeae90dd565f47775ef3470c
Jide Adebiyi <jeedeyy@gmail.com>
2020-07-13 19:14:10 +0100

    Update Pos_Neg.py

--- 01/Pos_Neg.py
+++ 01/Pos_Neg.py
@@ -1,13 +1,14 @@
-print('Hi there')
-print('insert number:')
+print('Hi there')     #say hi :)
+print('insert number:')   #ask user to insert number
 
-num = float(input())
+num = float(input())   #float because user may input a decimal
 
+#if statement to state out conditions
 if num > 0:
-    print('the number is a POSITIVE number')
+    print('the number is a POSITIVE number')   #if number input is greater than zero then its definitely positive
 elif num < 0:
-    print('the number is a NEGATIVE number')
+    print('the number is a NEGATIVE number')   #if number input is less than zero then its definitely negative
 else:
-    print('number is equal to ZERO!')
+    print('number is equal to ZERO!')          #if it is neither positive or negative then its value is ZER0
 
-print('Thank you!')
+print('Thank you!')                        #Thank you :)
