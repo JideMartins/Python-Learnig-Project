751a909ce453de68d4b8bf72e2b79e7db4c7ed87
Jide Adebiyi <jeedeyy@gmail.com>
2020-07-18 14:13:47 +0100

    Update Pos_Neg.py

--- 01/Pos_Neg.py
+++ 01/Pos_Neg.py
@@ -12,3 +12,24 @@ else:
     print('number is equal to ZERO!')          #if it is neither positive or negative then its value is ZER0
 
 print('Thank you!')                        #Thank you :)
+
+
+#New additional code combines activity 01 and 02 to know if its either odd or even and either positive or negative
+print("New additional code combines activity 01 and 02 to know if its either odd or even and either positive or negative")
+
+#ask user to insert value
+num = float(input('insert value:'))
+
+#if statement to state out conditions
+if num > 0 and num % 2==0:
+    print('the number is a POSITIVE, EVEN number')   
+elif num < 0 and num % 2 == 0:
+    print('the number is a NEGATIVE EVEN number')
+elif num > 0 and num % 2 !=0:
+    print('the number is a POSITIVE, ODD number')  
+elif num < 0 and num % 2 != 0:
+    print('the number is a NEGATIVE ODD number')    
+else:
+    print('number is equal to ZERO!')          
+
+print('cheers mate :)')
