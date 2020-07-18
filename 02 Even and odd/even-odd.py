d493ea23303974cc97b1c6894b36255130a23c83
Jide Adebiyi <jeedeyy@gmail.com>
2020-07-18 13:50:58 +0100

    Create even-odd.py

---
+++ 02 Even and odd/even-odd.py
@@ -0,0 +1,9 @@
+x = int(input())
+
+#conditional statement for checking even or odd number
+if x % 2 == 0:      #if the remainder of x divided by 2 is equal to zero
+    print('Number is EVEN')
+else:               #Else, definitely odd
+    print('Number is odd')
+
+print('Cheers Mate :)')
